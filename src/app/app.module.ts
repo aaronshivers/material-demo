91d0320f7b9e7f586d2377036d2cb00615ef9c9d
aaron shivers <aaorn@moonmail.org>
2019-11-13 11:03:09 -0600

    added buttons and typography components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { TypographyComponent } from './typography/typography.component';
+import { ButtonsComponent } from './buttons/buttons.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TypographyComponent,
+    ButtonsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ],
 })
-export class AppModule { }
+export class AppModule {
+}
